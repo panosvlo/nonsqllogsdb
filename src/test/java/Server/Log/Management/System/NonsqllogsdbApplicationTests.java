8bcc8b7c31919645cbfc05167f2f99d5fea9935f
panosvlo <panosvlontzos@hotmail.com>
2024-01-18 22:49:54 +0200

    Initial commit from spring initialz

---
+++ src/test/java/Server/Log/Management/System/NonsqllogsdbApplicationTests.java
@@ -0,0 +1,13 @@
+package Server.Log.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonsqllogsdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
