68df7f0e3822010253e346f79bf3a0e254bd1135
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2024-01-30 04:56:04 +0200

    Fix for the upvote API

--- src/main/java/gr/uoa/di/cs/nonsqllogsdb/service/UpvoteService.java
+++ src/main/java/gr/uoa/di/cs/nonsqllogsdb/service/UpvoteService.java
@@ -7,6 +7,10 @@ import gr.uoa.di.cs.nonsqllogsdb.repository.LogRepository;
 import gr.uoa.di.cs.nonsqllogsdb.repository.UpvoteRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,15 +22,21 @@ public class UpvoteService {
     @Autowired
     private LogRepository logRepository;
 
+
+
+    @Autowired
+    private MongoTemplate mongoTemplate;  // Inject MongoTemplate
+
     public void upvoteLog(String logId, String userId) {
         Upvote upvote = new Upvote();
         upvote.setLogId(new ObjectId(logId));
         upvote.setUserId(new ObjectId(userId));
         upvoteRepository.save(upvote);
 
-        Log log = logRepository.findById(logId).orElseThrow(() -> new RuntimeException("Log not found"));
-        log.setUpvoteCount(log.getUpvoteCount() + 1);
-        logRepository.save(log);
+        // Use MongoTemplate to update the upvote count
+        Query query = new Query(Criteria.where("_id").is(new ObjectId(logId)));
+        Update update = new Update().inc("upvoteCount", 1);
+        mongoTemplate.updateFirst(query, update, Log.class);
     }
 
     public List<ActiveAdminDTO> getTopFiftyActiveAdministrators() {
