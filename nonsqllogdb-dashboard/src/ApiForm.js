ff8a64bac4ba63e3acc2573adee8e0cdd82f6036
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2024-01-31 23:48:51 +0200

    Fixed the date picker in the queries

--- nonsqllogdb-dashboard/src/ApiForm.js
+++ nonsqllogdb-dashboard/src/ApiForm.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const ApiForm = ({ api, params, onParamChange, onSubmit }) => {
+  const [startDate, setStartDate] = useState(new Date());
   if (!api) return null;
 
   return (
     <form onSubmit={onSubmit}>
       <div className="api-description">{api.description}</div>
       {api.params.map((param, index) => (
-        <input
-          key={index}
-          type={param.type}
-          name={param.name}
-          placeholder={param.placeholder}
-          value={params[param.name]}
-          onChange={onParamChange}
-          required
-        />
+        param.type === 'date' ? (
+          <DatePicker
+            selected={params[param.name] ? new Date(params[param.name]) : startDate}
+            onChange={(date) => onParamChange({ target: { name: param.name, value: date.toISOString().split('T')[0] } })}
+            dateFormat="yyyy-MM-dd"
+            key={index}
+          />
+        ) : (
+          <input
+            key={index}
+            type={param.type}
+            name={param.name}
+            placeholder={param.placeholder}
+            value={params[param.name]}
+            onChange={onParamChange}
+            required
+          />
+        )
       ))}
       <button type="submit">Fetch Data</button>
     </form>
